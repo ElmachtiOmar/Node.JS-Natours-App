6cb05382faef9d5d5fdf9f0b2063da487f823599
ElmachtiOmar <elmachtiomar98@gmail.com>
2025-02-04 22:09:02 +0100

    NodeJS-Natours-APP

---
+++ models/userModel.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const validator = require('validator');
+
+const userShcema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A user must have a name!'],
+  },
+  email: {
+    type: String,
+    required: [true, 'A user must have an email!'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide us with a valid email'],
+  },
+  photo: {
+    type: String,
+  },
+  password: {
+    type: String,
+    required: [true, 'A user must have a password!'],
+    minlength: [8, 'A password must have 8 or more characters'],
+    select: false,
+  },
+  passwordConfirm: {
+    type: String,
+    require: [true, 'Please confirm you password'],
+    validate: {
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'Password should match password confirm',
+    },
+  },
+});
+
+userShcema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 12);
+  this.passwordConfirm = undefined;
+  next();
+});
+
+userShcema.methods.currectPassword = async function (
+  candidatePassword,
+  userPassword,
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
+
+const User = mongoose.model('User', userShcema);
+
+module.exports = User;
